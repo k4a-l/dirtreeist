2f59f0b7e6fb4167df9c76e2207e4b84429ac1ba
kasahala <4k.kasahara@gmail.com>
2026-02-23 16:10:41 +0900

    feat: export symbolSets

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { parse } from './modules/parse.js'
 import { convert } from './modules/convert.js'
 
 import { DirTree, Options } from './types/index'
+import { symbolSets } from 'constants/constant.js'
 
 const dirtreeist = (markdown: string, option?: Options) => {
   return parse(markdown).map((dirtree) => convert(dirtree, option))
 }
 
-export { parse, convert }
+export { parse, convert, symbolSets }
 export default dirtreeist
 export type { DirTree, Options }
