2b9380040b4cdb62b84edfb4a4ab4bb827158cb1
kasahala <4k.kasahara@gmail.com>
2026-02-23 16:52:36 +0900

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { parse } from './modules/parse.js'
 import { convert } from './modules/convert.js'
 
 import { DirTree, Options } from './types/index'
-import { symbolSets } from 'constants/constant.js'
+import { symbolSets } from './constants/constant.js'
 
 const dirtreeist = (markdown: string, option?: Options) => {
   return parse(markdown).map((dirtree) => convert(dirtree, option))
